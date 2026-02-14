e41395617c9e94702ae3ee88008770a34741b81f
Fayrouz El-Saadi <23-101044@students.eui.edu.eg>
2026-02-14 12:40:05 +0200

    nav

---
+++ src/components/layout/nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './nav.css';
+import Navbtn from '../navbtn';
+
+const nav = () => {
+    return ( 
+        <nav>
+            <Navbtn />
+        </nav>
+     );
+}
+ 
+export default nav;
